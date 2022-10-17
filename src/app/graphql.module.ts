14186e65a8366ba39bdf986d541f733d4a1b3919
Amin Abdelhedi <abdelhedi@vorwaerts.com>
2022-10-17 11:02:02 +0200

    fix: Move GitHub graphQL api token to environment variable

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -4,6 +4,7 @@ import { ApolloModule, Apollo, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache,ApolloLink } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
+import {environment} from "../environments/environment";
 
 const uri = 'https://api.github.com/graphql';
 
@@ -17,7 +18,7 @@ export function createApollo(httpLink: HttpLink) {
   const auth = setContext(() => {
     return {
       headers: {
-        Authorization: `bearer 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`
+        Authorization: environment.token,
       }
     };
   });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  token: 'bearer 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',
 };
